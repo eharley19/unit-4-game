193a3b265ebf2aff2e718ecc13c311a0c0672cb6
Edward Harley <edharley19@gmail.com>
2019-11-16 19:44:52 -0500

    score and reset working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,10 @@ var loss = 0;
 
 
 function reset() {
-    score = 0
+    score = 0;
+    $('#scorebox').html(`<h2>${score}</h2>`);
+    compNum = getRandomInt(19, 120);
+    $('#goalnum').html(`<h1>${compNum}</h1>`);
 
 }
 
@@ -35,6 +38,21 @@ console.log(val1, val2, val3, val4);
 function crystalScore(event) {
     score = score + event.data.val;
     $('#scorebox').html(`<h2>${score}</h2>`);
+    if (score == compNum) {
+        alert("You win!");
+        wins++;
+        console.log(wins);
+        $('#wins').text(wins);
+        reset();
+    }
+
+    if (score > compNum) {
+        alert("Sorry, try again.");
+        loss++;
+        console.log(loss);
+        $('#loss').text(loss);
+        reset();
+    }
 }
 
 $('#crystal1').click({val: val1}, crystalScore);
@@ -42,23 +60,8 @@ $('#crystal2').click({val: val2}, crystalScore);
 $('#crystal3').click({val: val3}, crystalScore);
 $('#crystal4').click({val: val4}, crystalScore);
 
-if (score === compNum) {
-    alert("You win!");
-    wins === wins++;
-    console.log(wins);
-
-    
-} 
 
 
     
-
-
-
-
-
-
-    
-
 
 });
